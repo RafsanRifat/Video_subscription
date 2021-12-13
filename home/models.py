02fa1ffc35cce13cfa5cf8c0744a3e636b868078
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-13 18:22:29 +0600

    ckeditor added to textfield

--- home/models.py
+++ home/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils.text import slugify
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 class Course(models.Model):
     course_name = models.CharField(max_length=80)
-    course_description = models.TextField()
+    course_description = RichTextField()
     is_premium = models.BooleanField(default=False)
     course_image = models.ImageField(upload_to='course.png')
     slug = models.SlugField(blank=True)
