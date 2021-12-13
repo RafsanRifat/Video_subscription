c3e304b981197b81b43d85a9b951ef9a06da3888
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-13 17:47:10 +0600

    Course model created and pillow installed

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=80)
+    course_description = models.TextField()
+    is_premium = models.BooleanField(default=False)
+    course_image = models.ImageField(upload_to='course.png')
+    slug = models.SlugField(blank=True)
