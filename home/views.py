45b14e8bc37f4b0cce1b5bfe98717c6afeeb65e9
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-15 16:06:13 +0600

    course_module view created

--- home/views.py
+++ home/views.py
@@ -8,3 +8,11 @@ def home(request):
     courses = Course.objects.all()
     context = {'courses': courses}
     return render(request, 'home.html', context)
+
+
+def view_course(request, slug):
+    course = Course.objects.filter(slug = slug).first()
+    course_module = CourseModule.objects.filter(course = course)
+
+    context = {'course': course, 'course_module': course_module }
+    return render(request, 'course.html', context)
